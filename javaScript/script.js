55e12a46a84cfb0478858303e52a769bc6979766
Kalix2006 <sirankathir@gmail.com>
2025-06-06 16:36:43 +0530

    6tcommit0606

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+console.log("hello")
+var a=10;
+var b=20;
+var total=a+b;
+console.log(a<b)
+if(a%2==0)
+{
+    console.log("even")
+}
+var arr=[10,20,30];
+var arr2=[...arr,40,50,60];
+console.log(arr)
+console.log(arr2)
+function sum()
+{
+    console.log(a+b)
+}
+var sum=()=>{
+    return(a+b)
+}
+console.log(sum())
+var marks=[90,89,58];
+var [m1,m2,m3]=marks;
+var venki=m1;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log("venki",venki)
+console.log(`${a}+${b}=${total}`)
